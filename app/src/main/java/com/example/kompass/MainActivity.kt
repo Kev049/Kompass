301aea6dce89988cc3d6ec163add3cb12abe0e55
Kevin Collins <nivek049@gmail.com>
2024-10-18 14:31:00 +0200

    Created a Nav bar

--- app/src/main/java/com/example/kompass/MainActivity.kt
+++ app/src/main/java/com/example/kompass/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.kompass
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -14,9 +16,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -41,12 +48,7 @@ class MainActivity : ComponentActivity() {
                         containerColor = Color.Blue,
                         contentColor = Color.Yellow,
                     ) {
-                        Text(
-                            modifier = Modifier.fillMaxSize(),
-                            text = "Navbar",
-                            textAlign = TextAlign.Center,
-                            fontSize = 10.em,
-                        )
+                        NavBarButtons()
                     }
                 }
             ) { innerPadding ->
@@ -61,6 +63,28 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun NavBarButtons() {
+
+//    Text(
+//        modifier = Modifier.fillMaxSize(),
+//        text = "Navbar",
+//        textAlign = TextAlign.Center,
+//        fontSize = 10.em,
+//    )
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        NavBarBtn("home", LocalContext.current)
+        NavBarBtn("qr", LocalContext.current)
+        NavBarBtn("user", LocalContext.current)
+        NavBarBtn("search", LocalContext.current)
+    }
+
 }
 
 @Composable
@@ -104,6 +128,51 @@ fun CategoryBtn(text: String, color: Color) {
             color = Color.White,
             fontSize = 18.sp,
             textAlign = TextAlign.Center
+        )
+    }
+}
+
+//@Composable
+//fun NavBarBtn(text: String) {
+//    Box(
+//        modifier = Modifier
+//            .width(60.dp)
+//            .height(60.dp)
+//            .background(color=Color.Black, shape = RoundedCornerShape(8.dp))
+//            .clickable { println("$text button Clicked") },
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Text(
+//            text = text,
+//            color = Color.White,
+//            fontSize = 12.sp,
+//            textAlign = TextAlign.Center
+//        )
+//    }
+//}
+
+@Composable
+fun NavBarBtn(text: String, context: Context) {
+    // var buttonPath = "R.drawable." + text + "_navbar_logo"
+
+    val drawableId = remember(text) {
+        context.resources.getIdentifier("${text}_navbar_logo", "drawable", context.packageName)
+    }
+
+    Box(
+        modifier = Modifier
+            .width(80.dp)
+            .height(80.dp)
+            .clickable { println("$text button Clicked") }
+            .background(color = Color.Black, shape = RoundedCornerShape(8.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        // TODO Change images so that we use larger images, to reduce bluriness by scaling
+        Image(
+            painter = painterResource(id = drawableId),
+            contentDescription = "$text icon",
+            modifier = Modifier.size(50.dp),
+            // contentScale = ContentScale.Fit
         )
     }
 }

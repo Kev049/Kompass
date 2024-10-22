036975a35c8a21a6a69fd01ab57235d5de08b26b
Lee4-M <liamn.mayor@gmail.com>
2024-10-22 21:31:01 +0200

    Fixed conflicts

--- app/src/main/java/com/example/kompass/ui/BasicInfoScreen.kt
+++ app/src/main/java/com/example/kompass/ui/BasicInfoScreen.kt
@@ -1,20 +1,13 @@
 package com.example.kompass.ui
 
 import androidx.compose.foundation.layout.Box
-<<<<<<< HEAD
 import androidx.compose.foundation.layout.PaddingValues
-=======
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
->>>>>>> 9b267aeee74af83d8caffb06ba4ef3d5d3202e8b
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.kompass.CategoryItem
 import com.example.kompass.NavButtons
 
@@ -22,8 +15,6 @@ import com.example.kompass.NavButtons
 fun BasicInfoScreen(
     innerPadding: PaddingValues
 ) {
-<<<<<<< HEAD
-=======
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -39,38 +30,11 @@ fun BasicInfoScreen(
 }
 
 
-@Composable
-fun CategoryBtn2(
-    categoryItem: CategoryItem, )
-{
->>>>>>> 9b267aeee74af83d8caffb06ba4ef3d5d3202e8b
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding),
-        contentAlignment = Alignment.Center
-    ) {
-        val categories = listOf(
-            CategoryItem.Dimensions, CategoryItem.Contents,
-            CategoryItem.Specific, CategoryItem.Materials
-        )
-        NavButtons(categories) {}
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
-<<<<<<< HEAD
-fun PreviewBasicInfoScreen() {
-    val defaultPadding = PaddingValues(0.dp)
-    BasicInfoScreen(innerPadding = defaultPadding)
-=======
 private fun InfoScreenPreview(
 ) {
-    KompassTheme {
-        BasicInfoScreen(
+    BasicInfoScreen(
             PaddingValues()
         )
-    }
->>>>>>> 9b267aeee74af83d8caffb06ba4ef3d5d3202e8b
 }

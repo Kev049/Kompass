3c98a112f7b798f7763ecdf9079c07cfee443dd3
Liam <liamn.mayor@gmail.com>
2024-10-23 10:57:37 +0200

    Clean up

--- app/src/main/java/com/example/kompass/MainActivity.kt
+++ app/src/main/java/com/example/kompass/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
